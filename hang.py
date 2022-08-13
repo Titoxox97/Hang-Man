61b0b735009271f1504c7de3333a346115b5a08f
Tito <matiasahrensdorf@gmail.com>
2022-08-13 19:47:47 -0400

    while loop to add conditions to random function

--- hang.py
+++ hang.py
@@ -3,5 +3,11 @@ from valid_words import val_words
 
 # -------------Function that will randomly choose a word from the list
 
-def get_vword(valid_words):
-    valid_words = random.choice(valid_words)
+def get_vword(val_words):
+    word = random.choice(val_words)
+
+    while '-' in word or ' ' in word:
+        word = random.choice(val_words)
+
+    return word
+
