e61313ec67024b0c923ce5377bbaf6f67850ed43
Tito <matiasahrensdorf@gmail.com>
2022-08-13 19:15:54 -0400

    Initial commit, create valid word list and pairing randomizer function

--- hang.py
+++ hang.py
@@ -1,3 +1,7 @@
 import random
 from valid_words import val_words
 
+# -------------Function that will randomly choose a word from the list
+
+def get_vword(valid_words):
+    valid_words = random.choice(valid_words)
