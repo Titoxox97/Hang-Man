62fcb8532de5cf914b727bc96c36e8b276c117e7
Tito <matiasahrensdorf@gmail.com>
2022-08-13 20:07:37 -0400

    define variables for components and create conditions for user inputs

--- hang.py
+++ hang.py
@@ -1,4 +1,6 @@
 import random
+import string
+
 from valid_words import val_words
 
 # -------------Function that will randomly choose a word from the list
@@ -11,3 +13,19 @@ def get_vword(val_words):
 
     return word
 
+
+def hang():
+    word = get_vword(val_words)
+    w_letters = set(word)   # ----- letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # ------- letters guessed
+
+    # -------------- User Input
+
+    user_guess = input('Guess a letter: ').upper()
+    # ------------ Condition that places the users guess among the other previously guessed letters
+    if user_guess in alphabet - used_letters:
+        used_letters.add(user_guess)
+    # ------------ Condition that indicates a user has guessed a letter in the word correctly
+        if user_guess in w_letters:
+            w_letters.remove(user_guess)
